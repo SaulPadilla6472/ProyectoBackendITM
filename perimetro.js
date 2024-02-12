a38a1b7960fbcd399e5a2b53949eabeec84f95cb
Pablo-A02 <126760626+Pablo-A02@users.noreply.github.com>
2024-02-12 13:58:27 -0600

    Create perimetro.js

---
+++ perimetro.js
@@ -0,0 +1,9 @@
+// Este es un código sencillo de que calcula el perimetro de un cuadrado 
+function perimetroCuadrado(lado) {
+    return lado * 4;
+}
+
+// Ejemplo de uso
+var lado = 5;
+var perimetro = perimetroCuadrado(lado);
+console.log("El perímetro del cuadrado con lado", lado, "es", perimetro);
